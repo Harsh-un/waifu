c9e18a4e9429c30d556a6ec35185b0076140388d
Fleynaro <dariusjohnson555@gmail.com>
2021-05-21 13:43:12 +0500

    venv removed

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Labs9\KR\Waifu\venv
+set VIRTUAL_ENV=R:\TG-waifu\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
