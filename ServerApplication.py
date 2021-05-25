93588273c9990468a2e51f226260984c41ba997a
Nikita <nike04@mail.ru>
2021-05-25 08:34:01 +0500

    настроить поиск осталось

---
+++ ServerApplication.py
@@ -0,0 +1,16 @@
+from Aggregators.ShikimoriAggregator import ShikimoriAggregator
+from UserSession import *
+
+
+class ServerApplication:
+    """Главный класс, который содержит сессии юзеров, агрегаторы"""
+    def __init__(self):
+        self.user_session = {}
+        self.shikimori_anime_agg = ShikimoriAggregator()
+        self.shikimori_manga_agg = ShikimoriAggregator()
+
+    def get_user_session(self, user_id: int):
+        """Получить сессию юзера"""
+        if user_id not in self.user_session:
+            self.user_session[user_id] = UserSession(user_id)
+        return self.user_session[user_id]
---
+++ UserSession.py
@@ -0,0 +1,10 @@
+from Aggregators.IAggregator import AbstractItemIterator
+
+
+class UserSession:
+    """Харнит всю требуемую информацию об одном юзере в течение сессии"""
+    def __init__(self, user_id: int):
+        self.user_id = user_id
+        self.cur_aggregator = None
+        self.cur_filter = None
+        self.cur_iterator = None
